77bfd63aae76a43eb2b1ef4ff22d4ef171fbf800
cyy5121212 <515822895@qq.com>
2013-11-12 00:25:17 +0800

    first git workflow

---
+++ 1039.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  fstream cin("1039.txt");
+  int a;
+  cin>>a;
+  cout<<"hello world!!!!!!"<<endl;
+  cout<<a<<endl;
+}
+

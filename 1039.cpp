fb2829684462f8bf87b1ed94fcc1f39ef149056c
cyy5121212 <515822895@qq.com>
2013-11-12 19:52:06 +0800

    直接对字符串做hash，省去了查找的时间。

--- 1039.cpp
+++ 1039.cpp
@@ -5,45 +5,50 @@
 #include <vector>
 #include <fstream>
 #include <algorithm>
+#include <string.h>
 using namespace std;
 
+
 int n, k;
-map<char *, vector<int> > m;
+const int big = 26 * 26 * 26 *10;
+vector<int> v[big];
 int main()
 {
-	fstream cin("1.txt");
-	cin>>n>>k;
+	//freopen("a.txt", "r", stdin);
+	//fstream cin("a.txt");
+	scanf("%d%d", &n, &k);
 	int a, b;
 	while(k--)
 	{
-		cin>>a>>b;
-		char tmp[5];
+		scanf("%d%d", &a, &b);
 		for (int i = 0; i < b; ++i)
 		{
+			char *tmp = new char[5];
 			scanf("%s", tmp);
-			m[tmp].push_back(a);
+			int hashIndex = (tmp[0] - 'A') * 26 * 26 * 10 + (tmp[1] - 'A') * 26 * 10 + (tmp[2] - 'A') * 10 + (tmp[3] - '0');
+			v[hashIndex].push_back(a);
 		}
 	}
 	std::vector<char *> query;
+
 	for (int i = 0; i < n; ++i)
 	{
-		char tmp[5];
+		char *tmp = new char[5];
 		scanf("%s", tmp);
 		query.push_back(tmp);
 	}
 	for (int i = 0; i < query.size(); ++i)
 	{
-		cout<<query[i];
-		if(m[query[i]].size() == 0)
-			cout<<" 0";
+		char *tmp = query[i];
+		int hashIndex = (tmp[0] - 'A') * 26 * 26 * 10 + (tmp[1] - 'A') * 26 * 10 + (tmp[2] - 'A') * 10 + (tmp[3] - '0');
+		printf("%s", query[i]);
+		if(v[hashIndex].size() == 0)
+			printf(" 0");
 		else
-			cout<<" "<<m[query[i]].size();
-		sort(m[query[i]].begin(), m[query[i]].end());
-		for (int j = 0; j < m[query[i]].size(); ++j)
-		{
-			cout<<" "<<m[query[i]][j];
-		}
-		cout<<endl;
+			printf(" %d", v[hashIndex].size());
+		sort(v[hashIndex].begin(), v[hashIndex].end());
+		for (int j = 0; j < v[hashIndex].size(); ++j)
+			printf(" %d", v[hashIndex][j]);
+		printf("\n");
 	}
 }
-

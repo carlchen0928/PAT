f4525b6d2b2c63860f8c5d0efc2da0f0a1460edf
cyy5121212 <515822895@qq.com>
2013-11-19 22:28:57 +0800

    complete 1047

--- 1047.cpp
+++ 1047.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string.h>
 using namespace std;
 
+struct stu
+{
+	char name[5];
+};
+
 int n, k;
-vector<vector<char *>> v;
+vector<vector<int> > v;
+vector<stu > names;
+
+bool cmp(int a1, int a2)
+{
+	return strcmp(names[a1].name, names[a2].name) < 0;
+}
 
 int main()
 {
 	freopen("a.txt", "r", stdin);
 	scanf("%d %d", &n, &k);
-	v = vector<vector<char *>>(n, vector<char *>());
+	v = vector<vector<int> >(k, vector<int>());
+	names = vector<stu>(n, stu());
 	for(int i = 0; i < n; i++)
 	{
-		char name[4];
 		int c;
-		scanf("%s %d", &name, &c);
-		int tmp;
+		scanf("%s %d", &names[i].name, &c);
 		for(int j = 0; j < c; j++)
 		{
+			int tmp;
 			scanf("%d", &tmp);
-			v[tmp].push_back(name);
+			v[tmp - 1].push_back(i);
 		}
 	}
 
 	for(int i = 0; i < v.size(); i++)
 	{
-		cout<<i + 1<<" "<<v[i].size()<<endl;
+		printf("%d %d\n", i + 1, v[i].size());
+		sort(v[i].begin(), v[i].end(), cmp);
 		for(int j = 0; j < v[i].size(); j++)
-			cout<<v[i][j]<<endl;
+			printf("%s\n", names[v[i][j]].name);
 	}
 }
